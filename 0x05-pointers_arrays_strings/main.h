c85b8522fc6627ef0b7a3e3ce08920655ee28d60
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-13 14:46:11 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef INV_MAIN_H
-#define INV_MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,4 +9,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-#endif
