654a46cf08397b40d338e73b47b93fec00c0a5ac
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-13 16:58:06 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a  string to stdout
+ * puts2 - prints every other character of a  string starting with first character
  * @str: string to print
  *
  * Return: void
@@ -9,11 +9,10 @@
 void puts2(char *str)
 {
 	int i = 0;
-
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
-	_putchar('\n');
+for(int i = 0; i < strlen(input); i+=2)
+{
+    putchar(input[i]);
 }
+}
+
+
