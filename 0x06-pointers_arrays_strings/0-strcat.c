18d5b34f41f03344732e148cd025aedb3c734f82
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-16 13:06:56 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - appends src to the dest 
+ * _strcat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
  *
- * Return: dest address
+ * Return: address of dest
  */
 char *_strcat(char *dest, char *src)
 {
