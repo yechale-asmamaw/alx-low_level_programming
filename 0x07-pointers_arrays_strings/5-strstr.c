46f19c958887773fc500bb7adccc7de6a15f3da0
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-20 14:28:30 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,13 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return (0);
 }
-
+/**
+ * coincidence - define if the string b is inside a.
+ * @a: source string
+ * @b: string to be searched
+ *
+ * Return: 1 if there is coincidence, otherwise 0.
+ */
 int coincidence(char *a, char *b)
 {
 	while (*b && *b == *a)
