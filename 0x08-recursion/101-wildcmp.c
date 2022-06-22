e116f3d1e3ee820caaca83794d8e434f1210c567
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-22 12:34:18 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 
+ * wildcmp - compares two strings and returns 1
  * @s1: string to compare to
  * @s2: string with wild character
  *
