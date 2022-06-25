b3a4b3f20b9a41d94a618344fa84d97b5b7fd931
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-25 14:37:20 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,32 +1,15 @@
 #include "main.h"
+
 /**
- * _atoi - converts a string to an integer.
- * @s: the string to convert
- * Return: the converted string.
+ * _atoi - tbd
+ *
+ * @s: parameter defined in main, pointer to memory
+ *
+ * Return: 0 for now
  */
+
 int _atoi(char *s)
 {
-	short boolean;
-	int i, minus, result;
-
-	i = minus = result = boolean = 0;
-	minus = -1;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			minus *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result -= (s[i] - '0');
-			boolean = 1;
-		}
-		else if (boolean == 1)
-			break;
-		i++;
-	}
-	result *= minus;
-	return (result);
+	s = s + 1;
+	return (0);
 }
