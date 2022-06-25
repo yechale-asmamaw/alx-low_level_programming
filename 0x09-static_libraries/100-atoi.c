f9b52ce5201b9d6354c1ce0a47b17fba1c68bfaa
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-25 14:41:10 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _atoi - tbd *
- * @s: parameter defined in main, pointer to memory
- * * Return: 0 for now
- */
-
+* _atoi - tbd 
+* @s: parameter defined in main, pointer to memory
+* Return: 0 for now
+*/
 int _atoi(char *s)
 {
 	s = s + 1;
