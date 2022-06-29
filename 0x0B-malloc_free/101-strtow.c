92b41f28acf86a5ff50e6e0b515fc23a751085e6
yechale-asmamaw <49897616+yechale-asmamaw@users.noreply.github.com>
2022-06-29 11:02:36 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,138 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
-/**
- * _strcmp - Like strcmp.
- * @s1: string.
- * @s2: string.
- * Return: int.
+/
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ * Return: no return
  */
-int _strcmp(char *s1, char *s2)
+void ch_free_grid(char **grid, unsigned int height)
 {
-	int i = 0;
-
-
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
-	}
-	return (0);
+  if (grid != NULL && height != 0)
+  {
+    for (; height > 0; height--)
+      free(grid[height]);
+    free(grid[height]);
+    free(grid);
+  }
 }
 
-/**
-  * _strlen - Find the lenght of a string.
-  * @s: String.
-  * @i: Position.
-  * Return: The lenght, integer.
-  */
-int _strlen(char *s, int i)
-{
-	int count = 0;
-
-	while (s[i] != ' ' && s[i] != '\0')
-	{
-		count++;
-		i++;
-	}
-	return (count);
-}
-
-/**
- * words - Count the numbers of words.
- * @str: String.
+/
+ * strtow - splits a string into words.
+ * @str: string.
  *
- * Return: Number of words.
- */
-int words(char *str)
-{
-	int count = 0, flag = 0;
-
-	while (*str)
-	{
-		if (*str != ' ')
-		{
-			flag = 1;
-		}
-		else if (flag == 1)
-		{
-			count++;
-			flag = 0;
-		}
-		str++;
-	}
-	return (count);
-}
-
-/**
- * _strcpy - Copy elements from a string to another.
- * @s: String.
- * @i: Position.
- * @tmp: Array where it's saved.
- * Return: The array whit the elements.
- */
-char *_strcpy(char *s, int i, char *tmp)
-{
-	int j;
-
-	for (j = 0; s[i] != ' ' && s[i] != '\0'; i++, j++)
-	{
-		tmp[j] = s[i];
-	}
-	tmp[j] = '\0';
-
-	return (tmp);
-}
-
-/**
- * strtow - Extract all the words from an string.
- * @str: String.
- *
- * Return: Array of words.
+ * Return: pointer of an array of integers
  */
 char **strtow(char *str)
 {
+  char **aout;
+  unsigned int c, height, i, j, a1;
 
-	int i = 0, j = 0, pos, t;
-	char **tmp;
-
-	if (str == NULL || _strcmp(str, "") || (words(str) == 0))
-	{
-		return (NULL);
-	}
-	tmp = malloc(sizeof(int *) * (words(str) + 1));
-	if (tmp == NULL)
-	{
-		return (NULL);
-	}
-	while (str[i])
-	{
-		if (str[i] != ' ')
-		{
-			pos = _strlen(str, i);
-			tmp[j] = malloc(sizeof(char) * (pos + 1));
-			if (tmp[j] == NULL)
-			{
-				for (t = j; t >= 0; t--)
-				{
-					free(tmp[t]);
-				}
-				free(tmp);
-				return (NULL);
-			}
-			_strcpy(str, i, tmp[j]);
-			j++;
-			i += pos;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	tmp[j] = NULL;
-	return (tmp);
+  if (str == NULL  *str == '\0')
+    return (NULL);
+  for (c = height = 0; str[c] != '\0'; c++)
+  {
+    if (str[c] != ' ' && (str[c + 1] == ' '  str[c + 1] == '\0'))
+      height++;
+  }
+  aout = malloc((height + 1) * sizeof(char *));
+  if (aout == NULL  height == 0)
+  {
+    free(aout);
+    return (NULL);
+  }
+  for (i = a1 = 0; i < height; i++)
+  {
+    for (c = a1; str[c] != '\0'; c++)
+    {
+      if (str[c] == ' ')
+        a1++;
+      if (str[c] != ' ' && (str[c + 1] == ' '  str[c + 1] == '\0'))
+      {
+        aout[i] = malloc((c - a1 + 2) * sizeof(char));
+        if (aout[i] == NULL)
+        {
+          ch_free_grid(aout, i);
+          return (NULL);
+        }
+        break;
+      }
+    }
+    for (j = 0; a1 <= c; a1++, j++)
+      aout[i][j] = str[a1];
+    aout[i][j] = '\0';
+  }
+  aout[i] = NULL;
+  return (aout);
 }
